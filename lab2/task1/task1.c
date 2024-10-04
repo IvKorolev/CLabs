0ac1173d9e59e2fa76429ad26e5f958d51d0f092
Ivan <mrvanivan@mail.ru>
2024-10-04 14:04:39 +0300

    init

---
+++ lab2/task1/task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+int main(int argc, char* argv[]){
+    if (argc != )
+    return 0;
+}
