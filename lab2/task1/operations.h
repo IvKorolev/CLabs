000afba30713510f65a3a7f39bb9055442a03cf2
Ivan <mrvanivan@mail.ru>
2024-10-07 00:20:11 +0300

    init 2.0

---
+++ lab2/task1/operations.h
@@ -0,0 +1,17 @@
+#ifndef CLABS_OPERATIONS_H
+#define CLABS_OPERATIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+enum errors{
+    INVALID_INPUT,
+    INVALID_MEMORY,
+    OK,
+};
+
+#endif //CLABS_OPERATIONS_H
--- lab2/task1/task1.c
+++ lab2/task1/task1.c
@@ -1,11 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
+#include "operations.h"
 
 int main(int argc, char* argv[]){
-    if (argc != )
+    if (argv[1][0] != '-' && (argv[1][2] == '\0')){
+        printf("Ошибка ввода флага\n");
+        return INVALID_INPUT;
+    }
+    switch(argv[1][1]){
+        case 'l':
+            break;
+        case 'r':
+            break;
+        case 'u':
+            break;
+        case 'n':
+            break;
+        case 'c':
+            break;
+        default:
+            printf("Ошибка: такого флага нет в доступных флагах: %s\n", argv[1]);
+            break;
+    }
     return 0;
 }
