678a37a3e10efbfb46139099428770e24c04063b
Ivan <mrvanivan@mail.ru>
2024-11-18 14:15:21 +0300

    little changes

--- lab4/task7/functions.c
+++ lab4/task7/functions.c
@@ -22,7 +22,7 @@ int compare(const void* a, const void* b){
 enum errors check_if_print(const char* buffer, MemoryCell** res, int size){
     if (strncmp(buffer, "print", 5) == 0 && strlen(buffer) == 6){
         for (int i = 0; i < size; i++){
-            printf("%d ", res[i]->value);
+            printf("%s: %d\n", res[i]->name, res[i]->value);
         }
         printf("\n");
         return OK;
@@ -43,13 +43,50 @@ enum errors check_if_print(const char* buffer, MemoryCell** res, int size){
     }
 }
 
-enum errors add_Cell(MemoryCell*** res, char* first_argument, long int num, int* size, int* capacity){
-    for (int i = 0; i < *size; i++){
-        if (strcmp((*res)[i]->name, first_argument) == 0){
-            (*res)[i]->value = num;
-            free(first_argument);
+enum errors search_value(MemoryCell** res, char* argument, int size, long int* resultation){
+    int left = 0;
+    int right = size - 1;
+    while (left <= right){
+        int mid = left + (right - left) / 2;
+        int result = strcmp(argument, res[mid]->name);
+        if (result == 0){
+            *resultation = res[mid]->value;
             return OK;
         }
+        if (result < 0){
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return NOT_DECLARED;
+}
+
+enum errors search_value_to_new(MemoryCell** res, char* argument, int size, long int* resultation){
+    int left = 0;
+    int right = size - 1;
+    while (left <= right){
+        int mid = left + (right - left) / 2;
+        int result = strcmp(argument, res[mid]->name);
+        if (result == 0){
+            res[mid]->value = *resultation;
+            return OK;
+        }
+        if (result < 0){
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return NOT_FOUND;
+}
+
+enum errors add_Cell(MemoryCell*** res, char* first_argument, long int num, int* size, int* capacity){
+    if (search_value_to_new(*res, first_argument, *size, &num) == OK){
+        free(first_argument);
+        return OK;
     }
 
     if (*size == *capacity){
@@ -74,27 +111,6 @@ enum errors add_Cell(MemoryCell*** res, char* first_argument, long int num, int*
     (*size)++;
     qsort(*res, *size, sizeof(MemoryCell*), compare);
     return OK;
-}
-
-enum errors search_value(MemoryCell** res, char* argument, int size, long int* resultation){
-    int left = 0;
-    int right = size - 1;
-    while (left <= right){
-        int mid = left + (right - left) / 2;
-        int result = strcmp(argument, res[mid]->name);
-        if (result == 0){
-            *resultation = res[mid]->value;
-            return OK;
-        }
-        if (result < 0){
-            right = mid - 1;
-        }
-        else{
-            left = mid + 1;
-        }
-    }
-    //фришить память
-    return NOT_DECLARED;
 }
 
 enum errors process_line(char* buffer, MemoryCell*** res, int* size, int* capacity){
@@ -159,6 +175,9 @@ enum errors process_line(char* buffer, MemoryCell*** res, int* size, int* capaci
             long int found;
             enum errors status_found = search_value(*res, second_argument, *size, &found);
             if (status_found != OK){
+                free(first_argument);
+                free(second_argument);
+                free(after_eq);
                 return NOT_DECLARED;
             }
             enum errors status_add = add_Cell(res, first_argument, found, size, capacity);
@@ -175,7 +194,13 @@ enum errors process_line(char* buffer, MemoryCell*** res, int* size, int* capaci
         else{
             status = search_value(*res, second_argument, *size, &first_to_op);
         }
-        if (status != OK) return status;
+        if (status != OK) {
+            free(first_argument);
+            free(second_argument);
+            free(third_argument);
+            free(after_eq);
+            return status;
+        }
 
         if (isdigit(third_argument[0]) || (third_argument[0] == '-' && isdigit(third_argument[1]))){
             status = str_to_int(third_argument, &second_to_op);
@@ -183,7 +208,13 @@ enum errors process_line(char* buffer, MemoryCell*** res, int* size, int* capaci
         else{
             status = search_value(*res, third_argument, *size, &second_to_op);
         }
-        if (status != OK) return status;
+        if (status != OK) {
+            free(first_argument);
+            free(second_argument);
+            free(third_argument);
+            free(after_eq);
+            return status;
+        }
 
         int resultat = 0;
         switch (operation){
@@ -226,7 +257,14 @@ enum errors process_file(FILE* input, MemoryCell*** res){
     while (fgets(buffer, 1023, input) != NULL){
         if (check_if_print(buffer, *res, size) == OK) continue;
         enum errors status_line = process_line(buffer, res, &size, &capacity);
-        if (status_line != OK) return status_line;
+        if (status_line == NOT_DECLARED) {
+            for (int i = 0; i < size; i++){
+                free((*res)[i]->name);
+                free((*res)[i]);
+            }
+            free(*res);
+            return NOT_DECLARED;
+        }
     }
 
     for (int i = 0; i < size; i++){
--- lab4/task7/operations.h
+++ lab4/task7/operations.h
@@ -12,6 +12,7 @@ enum errors{
     INVALID_MEMORY,
     OK,
     NOT_DECLARED,
+    NOT_FOUND,
 };
 
 typedef struct MemoryCell{
@@ -22,6 +23,7 @@ typedef struct MemoryCell{
 enum errors str_to_int(const char *x, long int *res);
 int compare(const void* a, const void* b);
 enum errors check_if_print(const char* buffer, MemoryCell** res, int size);
+enum errors search_value_to_new(MemoryCell** res, char* argument, int size, long int* resultation);
 enum errors add_Cell(MemoryCell*** res, char* first_argument, long int num, int* size, int* capacity);
 enum errors process_line(char* buffer, MemoryCell*** res, int* size, int* capacity);
 enum errors process_file(FILE* input, MemoryCell*** res);
