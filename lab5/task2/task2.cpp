d618343f9821613611946ce3a5a7dcec7c558101
Ivan <mrvanivan@mail.ru>
2024-12-16 17:56:19 +0300

    Some changes

--- lab5/task2/task2.cpp
+++ lab5/task2/task2.cpp
@@ -6,92 +6,114 @@
 #include <iterator>
 #include <filesystem>
 
-class encoder final{
-private:
-    std::vector<std::byte> key;
-    std::vector<int> s;
+std::ostream &operator<<(std::ostream &os, std::byte b)
+{
+    return os << char(std::to_integer<int>(b));
+}
 
-    void initialize_s(){
-        s.clear();
-        for (int i = 0; i < 256; ++i){
-            s.push_back(i);
+class encoder final
+{
+private:
+    std::vector<std::byte> S, _key;
+    int x, y;
+
+    int to_int(std::byte byte) { return std::to_integer<int>(byte); }
+
+    void init()
+    {
+        int key_length = _key.size();
+        for (int i = 0; i < 256; i++)
+        {
+            S[i] = std::byte((unsigned char)i);
         }
+
         int j = 0;
-        for (int i = 0; i < 256; ++i){
-            j = (j + s[i] + static_cast<int>(key[i % key.size()])) % 256;
-            std::swap(s[i], s[j]);
+        for (int i = 0; i < 256; i++)
+
+        {
+            j = (j + to_int(S[i]) + to_int(_key[i % key_length])) % 256;
+            std::swap(S[i], S[j]);
         }
     }
 
-    std::vector<std::byte> rc4(const std::vector<std::byte>& input){
-        initialize_s();
-        int i = 0, j = 0;
-        std::vector<std::byte> result(input.size());
-        for (size_t n = 0; n < input.size(); ++n) {
-            i = (i + 1) % 256;
-            j = (j + s[i]) % 256;
-            std::swap(s[i], s[j]);
-            int t = (s[i] + s[j]) % 256;
-            result[n] = input[n] ^ static_cast<std::byte>(s[t]);
-        }
-        return result;
+    std::byte get_s_item()
+
+    {
+        x = (x + 1) % 256;
+        y = (y + to_int(S[x])) % 256;
+
+        std::swap(S[x], S[y]);
+        return S[(to_int(S[x]) + to_int(S[y])) % 256];
     }
 
 public:
-    encoder(const std::vector<std::byte>& t) : key(t){
-        if (key.empty()){
-            throw std::invalid_argument("Encryption key cannot be empty");
-        }
+    encoder(std::vector<std::byte> &key) : S(256), _key(key), x(0), y(0)
+    {
+        if (key.size() == 0)
+            throw std::length_error("Zero key length");
     }
 
-    void set_key(const std::vector<std::byte>& new_key){
-        if (new_key.empty()){
-            throw std::invalid_argument("New encryption key cannot be empty");
-        }
-        key = new_key;
+    void set_key(std::vector<std::byte> const &key)
+    {
+        if (key.size() == 0)
+            throw std::length_error("Zero key length");
+        _key = key;
     }
 
-    void encode(const std::string& input_path, const std::string& output_path){
-
-        std::filesystem::path path_1{input_path};
-        std::filesystem::path path_2{output_path};
-
-        if (std::filesystem::equivalent(path_1, path_2))
+    void encode(std::string const &input_name, std::string const &output_name)
+    {
+        std::filesystem::path p1{input_name};
+        std::filesystem::path p2{output_name};
+        if (std::filesystem::equivalent(p1, p2))
         {
-            throw std::runtime_error("Equivalent file path");
+            throw std::runtime_error("Two identical files have been transferred");
         }
 
-        std::ifstream input_file(input_path, std::ios::binary);
-        if (!input_file.is_open()){
-            throw std::runtime_error("Failed to open input file: " + input_path);
+        std::ifstream in;
+        std::ofstream out;
+        in.open(input_name, std::ios::in);
+        if (!in.is_open())
+        {
+            in.close();
+            throw std::runtime_error("Couldn't open the input file");
+        }
+        out.open(output_name, std::ios::out);
+        if (!out.is_open())
+        {
+            in.close();
+            out.close();
+            throw std::runtime_error("Couldn't open the output file");
         }
 
-        std::vector<char> temp_data;
+        init();
+        x = 0, y = 0;
+
+        std::byte byte, checker{0x0};
         char ch;
-        while (input_file.get(ch)) {
-            temp_data.push_back(ch);
+        while ((ch = in.get()) != EOF)
+        {
+            byte = std::byte{(unsigned char)ch};
+            out << (byte ^ get_s_item());
         }
+        in.close();
+        out.close();
+    }
 
-        std::vector<std::byte> input_data;
-        for(char c : temp_data){
-            input_data.push_back(static_cast<std::byte>(c));
+    static std::vector<std::byte> generate_key(size_t size)
+    {
+        srand((unsigned)time(NULL));
+        std::vector<std::byte> key(size);
+        for (int i = 0; i < size; ++i)
+        {
+            key[i] = std::byte(rand() % 256);
         }
-        input_file.close();
-
-        auto output_data = rc4(input_data);
-
-        std::ofstream output_file(output_path, std::ios::binary);
-        if (!output_file.is_open()) {
-            throw std::runtime_error("Failed to open output file: " + output_path);
-        }
-
-        output_file.write(reinterpret_cast<const char*>(output_data.data()), output_data.size());
-        output_file.close();
+        return key;
     }
 };
 
 int main() {
-    std::vector<std::byte> key = {std::byte(1), std::byte(2), std::byte(3), std::byte(4), std::byte(5)};
+    std::vector<std::byte> key;
+    key = encoder::generate_key(100);
 
     encoder enc(key);
 
